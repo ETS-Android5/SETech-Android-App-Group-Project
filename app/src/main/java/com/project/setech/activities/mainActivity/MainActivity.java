bf68a31a123de1c3c7fa52af4e47bf0025602a2d
Do Hyun Lee <dlee432@aucklanduni.ac.nz>
2021-10-14 16:46:06 +1300

    partial implementation for recyclerView for MainActivity

---
+++ app/src/main/java/com/project/setech/activities/mainActivity/MainActivity.java
@@ -0,0 +1,113 @@
+package com.project.setech.activities.mainActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.project.setech.R;
+import com.project.setech.activities.listActivity.ListActivity;
+import com.project.setech.activities.listActivity.listRecyclerView.ListViewAdapter;
+import com.project.setech.activities.mainActivity.mainRecyclerView.MainListViewAdapter;
+import com.project.setech.model.IItem;
+import com.project.setech.model.Item;
+import com.project.setech.model.ItemFactory;
+import com.project.setech.util.CategoryType;
+import com.project.setech.util.Util;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button cpuButton;
+    private Button gpuButton;
+    private Button motherboardButton;
+
+    private RecyclerView recyclerView;
+    private MainListViewAdapter mainViewAdapter;
+    private List<IItem> topItemsList;
+
+    private FirebaseFirestore db= FirebaseFirestore.getInstance();
+    private CollectionReference ref = db.collection("Categories");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cpuButton = findViewById(R.id.cpuButton);
+        gpuButton = findViewById(R.id.gpuButton);
+        motherboardButton = findViewById(R.id.motherboardButton);
+
+        topItemsList= new ArrayList<>();
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+
+        int columns= 2;
+
+        cpuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToListActivity(CategoryType.CPU);
+            }
+        });
+
+        gpuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToListActivity(CategoryType.GPU);
+            }
+        });
+
+        motherboardButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToListActivity(CategoryType.Motherboard);
+            }
+        });
+    }
+
+    protected void onStart() {
+        super.onStart();
+
+        if(!topItemsList.isEmpty()){
+            return;
+        }
+        ItemFactory itemFactory= new ItemFactory();
+        CategoryType type= (CategoryType) getIntent().getSerializableExtra("CategoryType");
+        /*List<Item> topItems = retrieveTopItems();
+        for(Item i: topItems){
+            List<Integer> formattedImagePaths= Util.formatDrawableStringList((List<String>) i.get("images"),MainActivity.this);
+            Map<String,String> specifications = (Map<String, String>) i.get("specifications");
+            IItem newItem = itemFactory.createItem(i.getString("name"), formattedImagePaths,i.getString("price"),specifications,type);
+
+        itemsList.add(newItem);
+
+        mainViewAdapter = new MainListViewAdapter(MainActivity.this, itemsList,type);
+        recyclerView.setAdapter(mainViewAdapter);
+        mainViewAdapter.notifyDataSetChanged();
+        }*/
+    }
+
+    public void goToListActivity(CategoryType type) {
+        Intent newIntent = new Intent(MainActivity.this, ListActivity.class);
+        newIntent.putExtra("CategoryType", type);
+        startActivity(newIntent);
+        finish();
+    }
+
+   // public List<Item> retrieveTopItems(){
+        //return topItems;
+        // add a counter every time an item gets clicked and then return the top 3 items
+        // that has been clicked the most. Maybe in ListActivity?
+
+    //}
+}
