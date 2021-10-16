79bb0098189d4ac7dd185b07ff33f92bd0e23786
yli667 <69701294+yli667@users.noreply.github.com>
2021-10-16 16:47:15 +1300

    Allow users to search again after a search

--- app/src/main/java/com/project/setech/activities/searchActivity/SearchActivity.java
+++ app/src/main/java/com/project/setech/activities/searchActivity/SearchActivity.java
@@ -1,17 +1,22 @@
 package com.project.setech.activities.searchActivity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.project.setech.R;
 import com.project.setech.activities.listActivity.listRecyclerView.ListViewAdapter;
+import com.project.setech.activities.mainActivity.MainActivity;
 import com.project.setech.activities.searchActivity.searchRecyclerView.SearchViewAdapter;
 import com.project.setech.model.IItem;
 import com.project.setech.model.ItemFactory;
@@ -76,5 +81,37 @@ public class SearchActivity extends AppCompatActivity {
                 searchViewAdapter.notifyDataSetChanged();
             }
         });
+    }
+
+    public void goToSearchActivity(String searchString) {
+        Intent newIntent = new Intent(SearchActivity.this, SearchActivity.class);
+        newIntent.putExtra("SearchString", searchString);
+        startActivity(newIntent);
+        finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu, menu);
+
+        MenuItem menuItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+        searchView.setQueryHint("Type here to search");
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                goToSearchActivity(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
