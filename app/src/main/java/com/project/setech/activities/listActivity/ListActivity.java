c2d5e33d1a12ddf721039eacd87ffe5db1e9a584
DanilLinkov <danil.linkov@gmail.com>
2021-10-24 18:33:31 +1300

    fixing bug

--- app/src/main/java/com/project/setech/activities/listActivity/ListActivity.java
+++ app/src/main/java/com/project/setech/activities/listActivity/ListActivity.java
@@ -191,8 +191,6 @@ public class ListActivity extends ParentActivity {
         searchView = (SearchView) menuItem.getActionView();
         searchView.setMaxWidth(Integer.MAX_VALUE);
         searchView.setQueryHint("Type here to search");
-        EditText test = searchView.findViewById(R.id.search_src_text);
-        test.setInputType(InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS);
 
         //close button("x") on the search view and its listener
         View closeButton = searchView.findViewById(R.id.search_close_btn);
@@ -239,6 +237,7 @@ public class ListActivity extends ParentActivity {
                     listViewAdapter.setButtonAndString(order, clickedString);
                     listViewAdapter.backToFull();
                 }
+                Log.d("test", newText + " =========");
                 return false;
             }
         });
